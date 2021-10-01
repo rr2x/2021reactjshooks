3bdc46bf82e4d25e0e93f78b0308af9ebc0e128a
rr2x <79620740+rr2x@users.noreply.github.com>
2021-10-01 14:10:31 +0800

    useState with array, useEffect usages

--- h/src/App.js
+++ h/src/App.js
@@ -1,15 +1,23 @@
 import './App.css';
 
+
+
 // import ClassCounter from './c1/ClassCounter'
 // import HookCounter from './c1/HookCounter'
 // import HookCounterTwo from './c2/HookCounterTwo'
+// import HookCounterThree from './c3/HookCounterThree'
+// import HookCounterFour from './c4/HookCounterFour'
+// import HookCounterOne from './c5/HookCounterOne'
+import HookMouse from './c6/HookMouse';
 
-import HookCounterThree from './c3/HookCounterThree'
 
 function App() {
   return (
     <div className="App">
-      <HookCounterThree/>
+      <HookMouse/>
+      {/* <HookCounterOne/> */}
+      {/* <HookCounterFour /> */}
+      {/* <HookCounterThree/> */}
       {/* <HookCounterTwo/> */}
       {/* <HookCounter/> */}
       {/* <ClassCounter /> */}
